11231ad583ec2be0a2175c237843ff748484beb5
ehime <dodomeki@gmail.com>
2016-07-12 04:41:24 -0700

    Hello werld

--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
-<?php echo 'php working';
+<?php
+USE \Psr\Http\Message\ServerRequestInterface    AS Request;
+USE \Psr\Http\Message\ResponseInterface         AS Response;
+
+require 'vendor/autoload.php';
+
+$app = New \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
