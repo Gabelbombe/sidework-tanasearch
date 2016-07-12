c4a5330e017faef36f47d51cd777b04f2c5b9278
ehime <dodomeki@gmail.com>
2016-07-12 05:29:30 -0700

    Working controller with config located in ~/

--- public/index.php
+++ public/index.php
@@ -3,12 +3,22 @@ USE \Psr\Http\Message\ServerRequestInterface    AS Request;
 USE \Psr\Http\Message\ResponseInterface         AS Response;
 
 require dirname(__DIR__). '/vendor/autoload.php';
+$config = '/home/ubuntu/config.json';
 
-$app = New \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
+if (file_exists($config))
+{
+    $app = New \Slim\App([
+        'settings' => json_decode(file_get_contents($config), 1) ## requires array =/
+    ]);
 
-    return $response;
-});
-$app->run();
+    $app->get('/hello/{name}', function (Request $request, Response $response)
+    {
+        $name = $request->getAttribute('name');
+        $response->getBody()->write("Hello, $name");
+        return $response;
+    });
+
+    $app->run();
+} else { die('Failed to locate config...'); }
+
+
